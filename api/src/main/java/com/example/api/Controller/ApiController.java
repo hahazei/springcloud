9912571d622d417ae2689a748502dc49bcad303a
xuw-e <xuw-e@glodon.com>
2017-09-25 18:34:25 +0800

    config server

--- api/src/main/java/com/example/api/Controller/ApiController.java
+++ api/src/main/java/com/example/api/Controller/ApiController.java
@@ -1,5 +1,7 @@
 package com.example.api.Controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,11 +9,20 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * Created by xuwei on 2017/9/25.
  */
+@RefreshScope
 @RestController
 public class ApiController {
+
+    @Value("${from}")
+    private String from;
 
     @RequestMapping(value = "/go", method = RequestMethod.GET)
     public String go() {
         return "ok";
+    }
+
+    @RequestMapping(value = "/from", method = RequestMethod.GET)
+    public String from() {
+        return from;
     }
 }
